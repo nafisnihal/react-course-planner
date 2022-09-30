c807cce7aff66f7c0870872df584b18b9b667384
nafisnihal <nafisnihal12@gmail.com>
2022-09-30 23:36:56 +0600

    qanda

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Plan from './components/Plan/Plan';
+import QandA from './components/QandA/QandA';
 
 function App() {
   return (
     <div className='planner'>
       <Header></Header>
       <Plan></Plan>
+      <QandA></QandA>
     </div>
   );
 }
--- src/components/QandA/QandA.js
+++ src/components/QandA/QandA.js
@@ -3,10 +3,10 @@ import './QandA.css'
 
 const QandA = () => {
     return (
-      <div>
+      <div className="qna">
         <div>
-          <h2>How React works?</h2>
-          <p>
+          <h2 className="question">How React works?</h2>
+          <p className="ans">
             {" "}
             React uses a declarative paradigm that makes it easier to reason
             about your application and aims to be both efficient and flexible.
@@ -16,15 +16,21 @@ const QandA = () => {
           </p>
         </div>
         <div>
-          <h2>Difference between props and state?</h2>
-          <p>
+          <h2 className="question">Difference between props and state?</h2>
+          <p className="ans">
             Props are used to pass data from one component to another. The state
             is a local data storage that is local to the component only and
             cannot be passed to other components. The this.setState property is
             used to update the state values in the component
           </p>
         </div>
-        <div></div>
+        <div>
+          <h2 className="question">Where do we use USeEffect?</h2>
+          <p className="ans">
+            UseEffect can be used in many cases like updating the DOM, and
+            timers,runs on every render, fetching data.
+          </p>
+        </div>
       </div>
     );
 };
