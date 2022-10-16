107119b0e3e3b27c1470910c324a8ce63f037345
nafisnihal <nafisnihal12@gmail.com>
2022-10-17 01:47:57 +0600

    final

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Plan from './components/Plan/Plan';
-import QandA from './components/QandA/QandA';
 
 function App() {
   return (
